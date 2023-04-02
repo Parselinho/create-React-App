9f5a4fdce8ab61555d86ccc39898b5fdf166f5bc
Gal Parselani <Parselinho@gmail.com>
2023-04-02 18:49:53 +0300

    Update Gallery.js

--- create-react/src/components/Gallery.js
+++ create-react/src/components/Gallery.js
@@ -1,36 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect } from 'react';
 import Photo from './Photo';
-import apiKey from '../config';
 
-function Gallery({ searchTerm }) {
-  const [photos, setPhotos] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-
+function Gallery({ searchTerm, photos, loading, error, onSearchTermChange }) {
   useEffect(() => {
-    setLoading(true);
-    setError(null);
-
-    axios.get('https://api.flickr.com/services/rest/', {
-      params: {
-        method: 'flickr.photos.search',
-        api_key: apiKey,
-        text: searchTerm,
-        per_page: 12,
-        format: 'json',
-        nojsoncallback: 1
-      }
-    })
-    .then(response => {
-      setLoading(false);
-      setPhotos(response.data.photos.photo);
-    })
-    .catch(error => {
-      setLoading(false);
-      setError('An error occurred while fetching data from Flickr API');
-    });
-  }, [searchTerm]);
+    if (searchTerm) {
+      onSearchTermChange(searchTerm);
+    }
+  }, [searchTerm, onSearchTermChange]);
 
   return (
     <>
