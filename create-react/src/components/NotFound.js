1f4b385e9324a749fed2e29e89227b346dc23633
Gal Parselani <Parselinho@gmail.com>
2023-04-02 19:41:37 +0300

    Update NotFound.js

--- create-react/src/components/NotFound.js
+++ create-react/src/components/NotFound.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 function NotFound() {
   return (
     <div className="not-found">
+      <img src="/error.jpg" alt="Logo" className="not-found-logo" />
       <h1 className="not-found-heading">Oops! Page not found.</h1>
       <p className="not-found-text">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
       <Link to="/" className="not-found-link">Back to Home</Link>
