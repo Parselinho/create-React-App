3aa4e9c0b7a16ff76fa54888f818dcea6df53e49
Gal Parselani <Parselinho@gmail.com>
2023-04-02 19:26:02 +0300

    Update Gallery.js

--- create-react/src/components/Gallery.js
+++ create-react/src/components/Gallery.js
@@ -2,12 +2,14 @@ import React, { useEffect } from 'react';
 import Photo from './Photo';
 
 function Gallery({ searchTerm, photos, loading, error, onSearchTermChange }) {
+  // Call the onSearchTermChange function whenever the searchTerm changes
   useEffect(() => {
     if (searchTerm) {
       onSearchTermChange(searchTerm);
     }
   }, [searchTerm, onSearchTermChange]);
 
+  // Render the gallery component with photos and/or error message
   return (
     <>
       {loading ? (
