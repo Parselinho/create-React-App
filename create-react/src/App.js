70a22d53524416e6e57f766ac83019f3208df502
Gal Parselani <Parselinho@gmail.com>
2023-04-02 18:49:51 +0300

    Update App.js

--- create-react/src/App.js
+++ create-react/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
+import apiKey from './config';
 
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
@@ -7,18 +9,50 @@ import Gallery from './components/Gallery';
 import NotFound from './components/NotFound';
 
 function App() {
+  const [photos, setPhotos] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('sunsets');
+
+  useEffect(() => {
+    setLoading(true);
+    setError(null);
+
+    axios.get('https://api.flickr.com/services/rest/', {
+      params: {
+        method: 'flickr.photos.search',
+        api_key: apiKey,
+        text: searchTerm,
+        per_page: 24,
+        format: 'json',
+        nojsoncallback: 1
+      }
+    })
+    .then(response => {
+      setLoading(false);
+      setPhotos(prevPhotos => ({
+        ...prevPhotos,
+        [searchTerm]: response.data.photos.photo,
+      }));
+    })
+    .catch(error => {
+      setLoading(false);
+      setError('An error occurred while fetching data from Flickr API');
+    });
+  }, [searchTerm]);
+
   return (
     <BrowserRouter>
       <div className="container">
-        <SearchForm />
+        <SearchForm onSubmit={setSearchTerm} />
         <Nav />
         <Routes>
-          <Route path="/" element={<Gallery searchTerm="sunsets" />} />
-          <Route path="/search" element={<Gallery />} />
-          <Route path="/search/:searchTerm" element={<Gallery />} />
-          <Route path="/cats" element={<Gallery searchTerm="cats" />} />
-          <Route path="/dogs" element={<Gallery searchTerm="dogs" />} />
-          <Route path="/computers" element={<Gallery searchTerm="computers" />} />
+          <Route path="/" element={<Gallery searchTerm="sunsets" photos={photos["sunsets"] || []} loading={loading} error={error} onSearchTermChange={setSearchTerm} />} />
+          <Route path="/search" element={<Gallery photos={photos[searchTerm] || []} loading={loading} error={error} onSearchTermChange={setSearchTerm} />} />
+          <Route path="/search/:searchTerm" element={<Gallery photos={photos[searchTerm] || []} loading={loading} error={error} onSearchTermChange={setSearchTerm} />} />
+          <Route path="/cats" element={<Gallery searchTerm="cats" photos={photos["cats"] || []} loading={loading} error={error} onSearchTermChange={setSearchTerm} />} />
+          <Route path="/dogs" element={<Gallery searchTerm="dogs" photos={photos["dogs"] || []} loading={loading} error={error} onSearchTermChange={setSearchTerm} />} />
+          <Route path="/computers" element={<Gallery searchTerm="computers" photos={photos["computers"] || []} loading={loading} error={error} onSearchTermChange={setSearchTerm} />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
